26cb7ab83a4ffd1da7b61984e7db90c8b0e3e6d9
Jacob <jacobm3@gmail.com>
2019-05-20 12:36:18 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ provider "tfe" {
   hostname = "${var.hostname}"
   token    = "${var.token}"
 }
+
+resource "tfe_workspace" "test" {
+  name         = "my-workspace-name"
+  organization = "${var.organization}"
+}
+
+
