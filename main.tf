cb6e1a32869a95663e415c540e4c20eb688d4627
Jacob <jacobm3@gmail.com>
2019-05-20 12:42:53 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,13 @@ provider "tfe" {
   token    = "${var.token}"
 }
 
-resource "tfe_workspace" "test" {
-  name         = "my-workspace-name"
+resource "tfe_workspace" "app5" {
+  name         = "jenkins-app5-dev"
   organization = "${var.organization}"
+  
+  vcs_repo {
+    identifier = "jacobm3/jenkins-app5-dev"
+    }
 }
 
 
